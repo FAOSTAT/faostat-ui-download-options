b19c0e2f7bef06aa1777e39635b6898bf88790e1
Kalimaha <guido.barbaglia@gmail.com>
2015-08-31 12:17:52 +0200

    Edit default settings for the options window.

--- src/js/start.js
+++ src/js/start.js
@@ -1,3 +1,4 @@
+/*global define*/
 define(['jquery',
         'handlebars',
         'text!fenix_ui_download_options/html/templates.hbs',
@@ -39,66 +40,108 @@ define(['jquery',
             null_values_id: 'null_values',
 
             button_label: translate.download_as,
-            header_label: translate.button
+            header_label: translate.button,
+
+            decimal_numbers_value: 2,
+            decimal_separator_value: '.',
+            thousand_separator_value: ',',
+            flags_value: true,
+            codes_value: true,
+            units_value: true,
+            null_values_value: false
 
         };
 
     }
 
-    OPTIONS.prototype.init = function(config) {
+    OPTIONS.prototype.init = function (config) {
 
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
         /* Fix the language, if needed. */
-        this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'E';
+        this.CONFIG.lang = this.CONFIG.lang !== null ? this.CONFIG.lang : 'E';
 
     };
 
-    OPTIONS.prototype.show_as_modal_window = function() {
+    OPTIONS.prototype.apply_configuration = function () {
 
-        /* this... */
-        var _this = this;
+        /* Variables. */
+        var that = this,
+            source,
+            template,
+            dynamic_data,
+            html;
 
         /* Load button template. */
-        var source = $(templates).filter('#modal_window_button').html();
-        var template = Handlebars.compile(source);
-        var dynamic_data = {
-            prefix: this.CONFIG.prefix,
-            ok: this.CONFIG.ok_button,
-            pdf: this.CONFIG.pdf_button,
-            csv: this.CONFIG.csv_button,
-            excel: this.CONFIG.excel_button,
-            decimal_separators: this.CONFIG.decimal_separators,
-            thousand_separators: this.CONFIG.thousand_separators,
-            decimal_numbers: this.CONFIG.decimal_numbers,
-            show_options: this.CONFIG.show_options,
+        source = $(templates).filter('#modal_window_button').html();
+        template = Handlebars.compile(source);
+        dynamic_data = {
             ok_button_label: 'OK',
             pdf_label: translate.pdf,
             csv_label: translate.csv,
+            ok: this.CONFIG.ok_button,
             show_label: translate.show,
+            prefix: this.CONFIG.prefix,
+            pdf: this.CONFIG.pdf_button,
+            csv: this.CONFIG.csv_button,
             excel_label: translate.excel,
             flags_label: translate.flags,
             comma_label: translate.comma,
             codes_label: translate.codes,
             units_label: translate.units,
             period_label: translate.period,
+            excel: this.CONFIG.excel_button,
+            show_options: this.CONFIG.show_options,
             null_values_label: translate.null_values,
+            decimal_numbers: this.CONFIG.decimal_numbers,
             decimal_numbers_label: translate.decimal_numbers,
+            decimal_separators: this.CONFIG.decimal_separators,
             modal_window_button_label: this.CONFIG.button_label,
             modal_window_header_label: this.CONFIG.header_label,
             decimal_separator_label: translate.decimal_separator,
-            thousand_separator_label: translate.thousand_separator
+            thousand_separators: this.CONFIG.thousand_separators,
+            thousand_separator_label: translate.thousand_separator,
+            decimal_numbers_value: this.CONFIG.decimal_numbers_value,
+            decimal_separator_comma_checked: this.CONFIG.decimal_separator_value === ',' ? 'checked' : '',
+            decimal_separator_period_checked: this.CONFIG.decimal_separator_value === '.' ? 'checked' : '',
+            thousand_separator_comma_checked: this.CONFIG.thousand_separator_value === ',' ? 'checked' : '',
+            thousand_separator_period_checked: this.CONFIG.thousand_separator_value === '.' ? 'checked' : '',
+            flags_checked: this.CONFIG.flags_value ? 'checked' : '',
+            codes_checked: this.CONFIG.codes_value ? 'checked' : '',
+            units_checked: this.CONFIG.units_value ? 'checked' : '',
+            null_values_checked: this.CONFIG.null_values_value ? 'checked' : ''
         };
-        var html = template(dynamic_data);
-        $('#' + _this.CONFIG.placeholder_id).html(html);
+        html = template(dynamic_data);
+        console.debug(this.CONFIG.decimal_numbers);
+        $('#' + that.CONFIG.placeholder_id).html(html);
 
-        /* Link listeners to buttons. */
-        $('#' + this.CONFIG.prefix + 'ok_button').click(function() {_this.download_listener(null);});
-        $('#' + this.CONFIG.prefix + 'csv_button').click(function() {_this.download_listener('csv');});
-        $('#' + this.CONFIG.prefix + 'pdf_button').click(function() {_this.download_listener('pdf');});
-        $('#' + this.CONFIG.prefix + 'excel_button').click(function() {_this.download_listener('xls');});
+    };
 
+    OPTIONS.prototype.show_as_modal_window = function () {
+
+        /* Variables. */
+        var that = this;
+
+        /* Apply configuration. */
+        this.apply_configuration();
+
+        /* Add listeners for change events. */
+        $('#' + this.CONFIG.prefix + 'null_values').change(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'unit').change(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'codes').change(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'flags').change(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'thousand_separator').click(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'thousand_separator_period').click(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'decimal_separator').click(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'decimal_separator_period').click(function () { that.option_changed_listener(); });
+        $('#' + this.CONFIG.prefix + 'decimal_numbers').change(function () { that.option_changed_listener(); });
+
+    };
+
+    OPTIONS.prototype.option_changed_listener = function () {
+        /*global amplify*/
+        amplify.publish(this.CONFIG.prefix + 'event', this.collect_user_selection());
     };
 
     /**
@@ -107,30 +150,23 @@ define(['jquery',
      * @param {String} output_format Format of the output: 'csv', 'xls', 'pdf', null
      * @returns {Object} An object describing the selection made by the user through this component.
      */
-    OPTIONS.prototype.collect_user_selection = function(output_format) {
-        this.CONFIG.user_selection.decimal_separator = $('#' + this.CONFIG.prefix + 'decimal_separator').val();
-        this.CONFIG.user_selection.thousand_separator = $('#' + this.CONFIG.prefix + 'thousand_separator').val();
+    OPTIONS.prototype.collect_user_selection = function (output_format) {
+        this.CONFIG.user_selection.decimal_separator = $('input[name="' + this.CONFIG.prefix + 'decimal_separator"]:checked').val();
+        this.CONFIG.user_selection.thousand_separator = $('input[name="' + this.CONFIG.prefix + 'thousand_separator"]:checked').val();
         this.CONFIG.user_selection.decimal_numbers = $('#' + this.CONFIG.prefix + 'decimal_numbers').val();
         this.CONFIG.user_selection.flags = $('#' + this.CONFIG.prefix + 'flags').is(':checked');
         this.CONFIG.user_selection.codes = $('#' + this.CONFIG.prefix + 'codes').is(':checked');
         this.CONFIG.user_selection.units = $('#' + this.CONFIG.prefix + 'units').is(':checked');
         this.CONFIG.user_selection.null_values = $('#' + this.CONFIG.prefix + 'null_values').is(':checked');
-        this.CONFIG.user_selection.output_format = output_format;
+        if (output_format) {
+            this.CONFIG.user_selection.origin = output_format;
+            this.CONFIG.user_selection.output_format = output_format;
+        }
         return this.CONFIG.user_selection;
     };
 
-    OPTIONS.prototype.get_radio_button = function(radio_button_code) {
+    OPTIONS.prototype.get_radio_button = function (radio_button_code) {
         return $('#' + this.CONFIG.prefix + radio_button_code);
-    };
-
-    /**
-     * This function publish an event through AmplifyJS. This event will be used by the component
-     * that will show/export the data.
-     *
-     * @param {String} output_format Format of the output: 'csv', 'xls', 'pdf', null
-     */
-    OPTIONS.prototype.download_listener = function(output_format) {
-        amplify.publish(this.CONFIG.prefix + 'event', this.collect_user_selection(output_format));
     };
 
     /**
@@ -141,8 +177,8 @@ define(['jquery',
      * @param {function} callback This function must take two objects as argument: user selection and data
      * @param {Object} callback_data Data to be downloaded/exported.
      */
-    OPTIONS.prototype.onDownload = function(callback_data, callback) {
-        amplify.subscribe(this.CONFIG.prefix + 'event', function(user_selection) {
+    OPTIONS.prototype.onDownload = function (callback_data, callback) {
+        amplify.subscribe(this.CONFIG.prefix + 'event', function (user_selection) {
             callback(user_selection, callback_data);
         });
     };
