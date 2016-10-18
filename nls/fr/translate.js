1b3f78bc18ea298b4702fc3f8243c7bdeb7103c8
simonemurzilli <simone.murzilli@fao.org>
2016-10-18 11:16:30 +0200

    added french translations

--- nls/fr/translate.js
+++ nls/fr/translate.js
@@ -1,31 +1,14 @@
-/*global define*/
 define({
-
-    /*
-    thousand_separator: 'Thousand Separator (Value)',
-    period: 'Period',
-    comma: 'Comma',
-    none: 'None',
-    show: 'Output Formatting Options',
-    flags: 'Symboles',
-    codes: 'Codes',
-    units: 'Unités',
-    null_values: 'Null Values',
-    output_type: 'Output Type',
-    table: 'Table',
-    pivot: 'Pivot'
-    */
-
-    thousand_separator: 'Thousand Separator (Value)',
+    thousand_separator: 'Séparateur des milles (Value)',
     period: 'Période',
     comma: 'Virgule',
     none: 'Rien',
-    show: 'Output Formatting Options',
+    show: "ptions de formatage de l’output",
     flags: 'Symboles',
     codes: 'Codes',
     units: 'Unités',
     null_values: 'Values nulles',
-    output_type: 'Output Type',
+    output_type: "Type d’output",
     table: 'Table',
     pivot: 'Pivot'
 
